b144140e34783615c8b9f0a61b810f2fa0a1c19e
yaswanthkumar <yaswanthkumarmunagapati@gmail.com>
2021-04-09 17:38:36 +0530

    SeleniumGrid

---
+++ src/remoteTest.java
@@ -0,0 +1,27 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class remoteTest {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		
+		
+		// DesiredCapabilities
+		
+		DesiredCapabilities dc=new DesiredCapabilities();
+		
+		dc.setBrowserName("chrome");
+		dc.setPlatform(Platform.WINDOWS);
+		
+		WebDriver driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),dc);
+
+		driver.get("http://google.com");
+	}
+
+}
